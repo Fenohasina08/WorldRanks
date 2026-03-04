b13605ae8902aa1070a4c500c2ae3ebe47f3dd8b
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-04 20:22:09 +0300

    feat: changement dans App.tsx(intefration du fecth not ok)

--- world-ranks/src/App.tsx
+++ world-ranks/src/App.tsx
@@ -30,10 +30,25 @@ function App() {
   if (error) return <p>{error}</p>;
 
   return (
-    <div className='p-8'>
-      <h1 className="text-3xl font-bold mb-6">World Ranks</h1>
-        <p>Nombre de pays chargés : {countries.length}</p>
-    </div>
+    <main className="p-8 bg-gray-50 min-h-screen">
+      <h1 className="text-3xl font-bold mb-8 text-gray-800">World Ranks</h1>
+      
+      {/* Grille de pays */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        {countries.map((country) => (
+          <div key={country.cca3} className="bg-white p-4 rounded-xl shadow-sm border border-gray-100 hover:shadow-md transition-shadow">
+            <img 
+              src={country.flags.svg} 
+              alt={country.name.common} 
+              className="w-full h-32 object-cover rounded-md mb-4" 
+            />
+            <h2 className="text-lg font-bold text-gray-900 mb-1">{country.name.common}</h2>
+            <p className="text-sm text-gray-600">🌍 Région : {country.region}</p>
+            <p className="text-sm text-gray-600">👥 Population : {country.population.toLocaleString()}</p>
+          </div>
+        ))}
+      </div>
+    </main>
   )
 }
 
