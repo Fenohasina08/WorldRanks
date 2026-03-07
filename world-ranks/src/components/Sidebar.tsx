3154f533c587bca24fecb020aa2140b47c8d551d
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-07 17:33:06 +0300

    feat: rearrangement des espaces

--- world-ranks/src/components/Sidebar.tsx
+++ world-ranks/src/components/Sidebar.tsx
@@ -1,8 +1,8 @@
 const Sidebar = () => {
     return (
-         <div className="flex items-center justify-center w-full h-full bg-red-400">
-    <h1 className="text-2xl font-bold text-gray-800">World Ranks</h1>
-  </div>
+    <div className="flex items-center justify-center w-full h-full bg-red-400">
+        <div className="bg-blue-300 w-[90%] h-[110%] mb-[80px]"></div>
+     </div>
     )
 }
 
