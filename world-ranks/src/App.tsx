8f0d9584d9474d797fcf295a47e341ccebd23b0a
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-07 15:42:20 +0300

    feat: division des espaces dans la page App

--- world-ranks/src/App.tsx
+++ world-ranks/src/App.tsx
@@ -1,1 +1,14 @@
- 
+  const App = () => {
+  return (
+    <div class = "w-full h-screen flex flex-col bg-amber-600">
+      <div class="w-full h-1/3 bg-green-300" >
+
+      </div>
+      <div class="w-full h-2/3 bg-blue-500">
+
+      </div>
+    </div>
+
+    )
+}
+export default App;
