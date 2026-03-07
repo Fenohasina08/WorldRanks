4a4c4c7c268055843dbe46d82e7f815c54a5fc7d
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-07 17:47:54 +0300

    feat: rearrangement des espaces

--- world-ranks/src/components/Sidebar.tsx
+++ world-ranks/src/components/Sidebar.tsx
@@ -1,7 +1,15 @@
 const Sidebar = () => {
     return (
     <div className="flex items-center justify-center w-full h-full bg-red-400">
-        <div className="bg-blue-300 w-[90%] h-[110%] mb-[80px]"></div>
+        <div className="bg-blue-300 w-[90%] h-[110%] mb-[80px] rounded-t-[30px] overflow-hidden">
+                <div className='w-[100%] h-[9.5%] bg-green-600 flex'>
+                    <div className="flex w-1/2 bg-blue-100"></div>
+                    <div className="flex w-1/2 bg-amber-900"></div>
+                </div>
+                <div className = 'w-[100%] h-[90.5%] bg-pink-300'>
+                
+                </div>
+        </div>
      </div>
     )
 }
