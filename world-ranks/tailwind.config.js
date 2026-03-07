38e9ebf68d8129af4d780d790dba7329345d44e4
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-07 11:30:57 +0300

    feat: create tailwindconfig.js

---
+++ world-ranks/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class', // 🌙 Cette ligne est la clé du Dark Mode !
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
