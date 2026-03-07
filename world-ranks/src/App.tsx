06c0af6f4eb9ff0676e5be88db2fcabb8b95b4fa
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-07 11:24:31 +0300

    feat: integrate Navbar into App layout

--- world-ranks/src/App.tsx
+++ world-ranks/src/App.tsx
@@ -1,13 +1,20 @@
- import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CountryDetail from './pages/CountryDetail';
+import Navbar from './components/Navbar'; // 1. On importe la Navbar
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/country/:id" element={<CountryDetail />} />
-    </Routes>
+    // 2. On ajoute un conteneur avec les classes de base pour le thème
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors">
+      {/* 3. On affiche la Navbar (avec des fonctions vides pour le moment) */}
+      <Navbar isDark={false} toggleDark={() => {}} /> 
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/country/:id" element={<CountryDetail />} />
+      </Routes>
+    </div>
   );
 }
 
