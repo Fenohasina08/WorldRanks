8d6e3d1c6a409da1291d601e1bcba0cad94596d2
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-07 16:02:34 +0300

    feat: fixation du tableau de bord

--- world-ranks/src/App.tsx
+++ world-ranks/src/App.tsx
@@ -3,8 +3,8 @@ const App = () => {
     <div className="flex flex-col w-full h-screen bg-amber-600">
       <div className="w-full bg-green-300 h-1/3"></div>
       <div className="flex items-center justify-center w-full bg-blue-500 h-2/3">
-        <div className="flex items-center justify-center h-[90%] w-[96%] bg-violet-700 
-        mb-[250px]"></div>
+        <div className="flex items-center justify-center h-[110%] w-[96%] bg-violet-700 
+        mb-[70px] rounded-t-[25px]"></div>
       </div>
     </div>
   );
